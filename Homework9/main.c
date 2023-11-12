71e4e7c124325fe6dfcf1ffa4739bb9f85b4baf4
Shlumpz <jaw062701@gmail.com>
2023-11-12 16:18:14 -0500

    added to main.c

--- Homework9/main.c
+++ Homework9/main.c
@@ -1,1 +1,45 @@
 #include "linked_list.h"
+
+//print the floats
+int print_float(const LinkedListNode* node)
+{
+    const float* data = (const float*)node -> data;
+    printf("%.2f, ", *data);
+
+    //continue iterating
+    return 1;
+}
+
+int main()
+{
+    //create linked list of float
+    LinkedList* floatList = llist_create(sizeof(float));
+
+    //append floats
+    float data0 = 1.1;
+    float data1 = 2.3;
+    float data2 = 4.5;
+    float data3 = 9.3;
+
+    llist_append(floatList, &data0);
+    llist_append(floatList, &data1);
+    llist_append(floatList, &data2);
+    llist_append(floatList, &data3);
+
+    //use _iterate to print to the list
+    printf("(");
+    llist_iterate(floatList, print_float);
+    printf(")\n");
+
+    //test other llist functions
+    size_t listSize = llist_size(floatList);
+    size_t totatSize = llist_total_size(llist_total_size(floatList));
+
+    printf("Size of list: \n", listSize);
+    printf("Total size of list: \n", totatSize);
+
+    //destroy 
+    llist_destroy(floatList);
+    
+    return 0;
+}
